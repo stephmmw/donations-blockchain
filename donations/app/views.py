adc279b9370a1cdd6b89ccf23784464ccf2fb1d6
stephmmw <stephenmmw@yahoo.com>
2023-10-25 09:45:49 +0300

    Add minimal interface

--- donations/app/views.py
+++ donations/app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+class Block():
+    pass
+
+# Interface
+def index(request):
+    if request.method == "POST":
+        timestamp = str(datetime.now().isoformat())
+        name = request.POST["name"]
+        amount = request.POST["amount"] 
+        
+        print('\nDonation \nName: '+ name + '\nAmount: ' + amount+ '\nTimestamp: ' + timestamp)    
+    
+    app = 'Donations'
+    context = {
+        "app": app,
+        "title": 'Donations',
+    }
+    template_name = "index.html"
+    return render(request, template_name, context)
